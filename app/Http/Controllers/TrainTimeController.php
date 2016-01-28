797954df79d54690946d4f61abe9c5128f6b2d30
Ivan Kolesnikov <impalago@yandex.ru>
2016-01-28 11:30:51 +0300

    The reply from data-traintime

--- app/Http/Controllers/TrainTimeController.php
+++ app/Http/Controllers/TrainTimeController.php
@@ -2,21 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\TrainTime;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class TrainTimeController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
+     * @param Request $request
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'endsta' => 'required|size:3',
+        ]);
+
+        if ($validator->fails()) {
+            $response = array('message' => $validator->errors(), 'status' => 400);
+            return response()->json($response)->setStatusCode(400, 'Bad Request');
+        }
+
+        $data = TrainTime::where('name', $request->endsta)
+            ->orderBy('created_at', 'desc')
+            ->first(['name', 'data']);
+
+        if(!$data) {
+            $response = array('message' => 'Data not found', 'status' => 404);
+            return response()->json($response)->setStatusCode(404, 'Data not found');
+        }
+
+        $response = array('name' => $data->name, 'data' => json_decode($data->data, true));
+        return response()->json($response)->header('Content-Type', 'json');
     }
 
     /**
